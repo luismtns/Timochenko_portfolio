35a64cd17a9fc7d8925a83ac3feabe6d1b60451e
Luis Otávio <luismtns@gmail.com>
2020-04-23 18:28:43 -0300

    Loader finalizado e emulado

--- Timochenko-app/src/app/loader/loader.component.ts
+++ Timochenko-app/src/app/loader/loader.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 import { LoaderState } from './loader.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-loader',
@@ -9,6 +10,9 @@ import { LoaderState } from './loader.model';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit, OnDestroy {
+
+  time_seconds:number = 13; //13
+  progress_percent:number = 0;
   show = true;
   private subscription: Subscription;
   constructor(private loaderService: LoaderService) { }
@@ -17,8 +21,39 @@ export class LoaderComponent implements OnInit, OnDestroy {
     .subscribe((state: LoaderState) => {
       this.show = state.show;
     });
+    
+    if(!environment.production){
+      const subscrive_timer = timer(1000, 1000).subscribe((val) =>{
+        if(val > this.time_seconds){
+          this.loaderService.hide();
+          subscrive_timer.unsubscribe()
+        }else{
+          var progress_value = (val / this.time_seconds);
+          if(progress_value > 1){
+            progress_value = 1;
+          }
+          this.updateProgress(progress_value)
+        }
+        
+      });
+
+    }else{
+      this.loaderService.hide();
+    }
+    
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+  
+  updateProgress (progressRatio) {
+    this.progress_percent = Math.floor(progressRatio*100);
+    console.log('progress_percent', this.progress_percent);
+    // }
+    if(progressRatio === 1){
+      console.log("Progress Ratio: ", 'finished');
+    }else{
+      console.log("Progress Ratio: ", 'not finished');
+    }
+  };
 }
--- Timochenko-app/src/app/main/Home/home-view/home-view.component.ts
+++ Timochenko-app/src/app/main/Home/home-view/home-view.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ElementRef, ViewChild } from '@angular/core';
 import { LoaderService } from 'src/app/services/loader.service';
 import { timer } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
   styleUrls: ['./home-view.component.scss']
 })
-export class HomeViewComponent implements OnInit {
-  time_seconds:number = 12;
-  // time_seconds:number = 0;
+export class HomeViewComponent implements OnInit  {
+
   point_list: any;
   point_interval:any;
   pink_text:boolean = false;
+
   constructor(private loaderService: LoaderService) { 
   }
 
-  ngOnInit(): void {
-    timer(this.time_seconds * 1000).subscribe(val =>{
-      this.loaderService.hide();
-    });
+  ngOnInit (): void {
 
     this.runPoints();
     this.point_interval = setInterval(() => {
       this.runPoints(); 
     }, 800);
 
+  }
+
+  ngOnDestroy():void{
+    clearInterval( this.point_interval)
+    this.loaderService.hide();
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
   runPoints() {
