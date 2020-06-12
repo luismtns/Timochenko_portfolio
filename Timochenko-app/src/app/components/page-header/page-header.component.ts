c735b1cef671ae33a092dfefe60f20eafea44902
Luis Bovo <luis.bovo@bit.com.br>
2020-06-12 15:18:14 -0300

    PageHeader transition ajust backgrounand configure mobile; Start Ajust Chill Mobile; General fixes mobile

--- Timochenko-app/src/app/components/page-header/page-header.component.ts
+++ Timochenko-app/src/app/components/page-header/page-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PageModule } from './page/page.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
@@ -8,10 +9,48 @@ import { PageModule } from './page/page.module';
 })
 export class PageHeaderComponent implements OnInit {
   @Input() page: PageModule;
+  page_count: Object[] = [
+    {
+      name: '/p/Chill',
+      number:1,
+      position: '0em',
+    },
+    {
+      name: '/p/Lit',
+      number:2,
+      position: '-2em',
+    },
+    {
+      name: '/p/Reebok',
+      number:3,
+      position: '-4em',
+    },
+    {
+      name: '/p/Senna',
+      number:4,
+      position: '-6em',
+    },
+    {
+      name: '/p/Palmeiras',
+      number:5,
+      position: '-8em',
+    },
+  ];
 
-  constructor() { }
+  top_number_cont:string = '0em'
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getNumberPosition(this.router.url);
+  }
+
+  
+  getNumberPosition(page){
+    var select_page = this.page_count.find(e=>e['name'] == page)
+    var top_em = select_page['position']
+    this.top_number_cont = top_em;
   }
 
 }
