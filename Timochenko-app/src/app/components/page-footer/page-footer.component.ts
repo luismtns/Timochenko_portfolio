08a391ee4b52e3959706d83d5177977184c208ec
Luis Bovo <luis.bovo@bit.com.br>
2020-06-23 15:10:31 -0300

    more fixes into page footer

--- Timochenko-app/src/app/components/page-footer/page-footer.component.ts
+++ Timochenko-app/src/app/components/page-footer/page-footer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MouseStyleService } from './../../services/mouse-style.service';
-import { debounce } from './../../decorators/debounce.decorator';
 
 @Component({
   selector: 'app-page-footer',
@@ -16,6 +15,7 @@ export class PageFooterComponent implements OnInit {
   mouse_top: number = 0;
   mouse_left: number = 0;
   mouse_active: boolean = false;
+  _visibility: boolean = false;
   pointer_click: boolean = false;
 
   @ViewChild('item') item: ElementRef;
@@ -42,14 +42,15 @@ export class PageFooterComponent implements OnInit {
   openRoute(){
     this.pointer_click = true;
     this._MouseStyleService.show();
-    setTimeout(()=> this.route.navigate([this.pageLink]),300)
+    setTimeout(()=> this.route.navigate([this.pageLink]),400)
   }
   mouseOverFooter(){
-    this.mouse_active = true;
+    this.mouse_active = true
     this._MouseStyleService.hide();
   }
   mouseLeaveFooter(){
     this.mouse_active = false
+    this._visibility = false;
     this._MouseStyleService.show();
   }
 
